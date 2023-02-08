e6b0beffd252da13c082d2af93a87088cee4545a
Pedro Nogueira <cmcg89034@gmail.com>
2023-02-08 10:56:36 -0300

    Back to this!

--- code/src/_main.cpp
+++ code/src/_main.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
+
 #include "PixelOperations.h"
+
+using namespace std;
 
 int main() {
   std::string imgDir = "../img/";
   std::string colorGirlImgName = "colorGirl.jpg";
 
+  cout << endl;
+
   enlargeBits(imgDir + colorGirlImgName, 2);
+
+  cout << endl;
 
   return 0;
 }
